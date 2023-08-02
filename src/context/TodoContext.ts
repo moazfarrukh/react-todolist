8dbb71176e023f27fa9b98fcfa8b2c1f5e94f8d9
moazfarrukh <maazfarrukh@gmail.com>
2023-08-02 17:51:11 +0500

    use Context

--- src/context/TodoContext.ts
+++ src/context/TodoContext.ts
@@ -1,5 +1,5 @@
 import { createContext } from "react"
-import {todoDataContextType, todoFuncContextType } from "../types/Todo";
+import {todoFuncContextType } from "../types/Todo";
 
 export const TodoFuncContext = createContext<todoFuncContextType| null>(null);
 
